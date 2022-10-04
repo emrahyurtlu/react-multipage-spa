a18b2f52fb50502ff9718464dfded40ec44ba267
Emrah Yurtlu <emrahyurtlu@gmail.com>
2022-10-04 23:05:42 +0300

    Welcome and Products pages are added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome" component={Welcome} />
+      <Route path="/products" component={Products} />
     </div>
   );
 }
